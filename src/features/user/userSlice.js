eee37c3539a7a867b164b8ddb16077a96f23e305
Mikyung Cho <jmk2633@gmail.com>
2025-08-03 23:19:13 +0900

    login

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,7 +6,18 @@ import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/login", { email, password });
+      if(response.status === 200){
+        sessionStorage.setItem("token", response.data.token);
+      }
+      return response.data;
+    } catch (error) {
+      // 실패시 생긴 에러값을 reducer에 저장
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -74,6 +85,18 @@ const userSlice = createSlice({
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.registrationError = action.payload;
+      })
+      .addCase(loginWithEmail.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithEmail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.loginError = null;
+      })
+      .addCase(loginWithEmail.rejected, (state, action) => {
+        state.loading = false;
+        state.loginError = action.payload;
       });
   },
 });
